925a68ae5d090c846a8f434277b87d886c2b56cb
Haoyang Gao <gaohaoyang126@126.com>
2024-11-11 23:01:24 -0500

    feat: 🎸 add path

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+
+const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
   images: { unoptimized: true },
+  assetPrefix: isProd ? '/works/' : '',
+  basePath: isProd ? '/works' : '',
 }
 
-export default nextConfig;
+export default nextConfig

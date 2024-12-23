44c5c6f05660a03d2dacc7a81d630cfcc14ebcef
Haoyang Gao <gaohaoyang126@126.com>
2024-12-23 13:12:30 -0500

    feat: 🎸 update

--- src/app/share/page.tsx
+++ src/app/share/page.tsx
@@ -1,8 +1,32 @@
+'use client'
+
 import React from 'react'
+import { Button } from '@nextui-org/button'
 
 const Share = () => {
   return (
-    <div>Share</div>
+    <div>
+      <Button
+        color="primary"
+        onClick={() => {
+          console.log('Share')
+          const params = {
+            funcName: 'openSystemShare',
+            params: {
+              title: 'title',
+              desc: 'desc',
+              link: 'https://gaohaoyang.github.io/works/copy-serious-business',
+              imgUrl:
+                'https://cdn.jsdelivr.net/gh/Gaohaoyang/pics2/assets/face.png',
+            },
+          }
+          console.log(params)
+          console.log(JSON.stringify(params))
+        }}
+      >
+        Share
+      </Button>
+    </div>
   )
 }
 

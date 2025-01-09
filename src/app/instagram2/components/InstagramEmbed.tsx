681681cff91972ab771152efba37ae0d353ebece
Haoyang Gao <gaohaoyang126@126.com>
2025-01-09 17:10:39 -0500

    style: 💄 update width property in InstagramEmbed component for consistent layout

--- src/app/instagram2/components/InstagramEmbed.tsx
+++ src/app/instagram2/components/InstagramEmbed.tsx
@@ -53,11 +53,10 @@ export function InstagramEmbed({
           border:0;
           border-radius:3px;
           box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15);
-          margin: 1px;
           max-width:540px;
           min-width:326px;
           padding:0;
-          width:99.375%;
+          width:100%;
         "
       ></blockquote>`
     }

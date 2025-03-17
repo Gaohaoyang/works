20b5fe38aab4befa1011649fc483ef28b1aa6b99
Haoyang Gao <gaohaoyang126@126.com>
2025-03-17 18:01:06 -0400

    chore: 🔧 update dependencies and remove unused ECharts component for cleaner codebase

--- src/app/ee/page.tsx
+++ src/app/ee/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import dynamic from 'next/dynamic'
+import Ads from '@/components/ads'
 
 // Dynamically import the ECharts component to avoid SSR issues
 const EChartsComponent = dynamic(() => import('./EChartsComponent'), {
@@ -8,10 +9,13 @@ const EChartsComponent = dynamic(() => import('./EChartsComponent'), {
 
 const EE = () => {
   return (
-    <div className="mx-auto min-h-screen p-4">
+    <div className="mx-auto min-h-screen p-4 pb-48">
       <h1 className="mb-4 text-2xl font-bold">Express Entry Draws</h1>
-      <div className="h-[calc(100vh-8rem)] w-full">
-        <EChartsComponent />
+      <div className="container mx-auto">
+        <Ads />
+        <div className="h-[600px] w-full bg-slate-100">
+          <EChartsComponent />
+        </div>
       </div>
     </div>
   )
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import { ThemeProvider } from '@/components/theme-provider'
+import { HeroUIProvider, ToastProvider } from '@heroui/react'
 
 export const metadata: Metadata = {
   title: 'Create Next App', // todo
@@ -15,14 +16,17 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="min-h-screen antialiased">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
+        <HeroUIProvider>
+          <ToastProvider />
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </HeroUIProvider>
       </body>
     </html>
   )

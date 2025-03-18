93ee00529b93ccd462ee787c2292f138f01541a9
Haoyang Gao <gaohaoyang126@126.com>
2025-03-17 22:58:04 -0400

    feat: 🎸 update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9139027259917346"
+          crossorigin="anonymous"
+        ></script>
+      </head>
       <body className="min-h-screen antialiased">
         <HeroUIProvider>
           <ToastProvider />
